2c633bae042f52f1be6ebfb3fd3898327a6ca5e2
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-09 16:25:03 +0200

    diseño sin layout

--- Journal/Journal-App/src/auth/pages/LoginPage.jsx
+++ Journal/Journal-App/src/auth/pages/LoginPage.jsx
@@ -1,10 +1,44 @@
+import { Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      LoginPage
-    </div>
+    <Grid container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{minHeight:'100vh',backgroundColor:'primary.main',padding:4}}
+
+    >
+      <Grid item
+      className='box-shadow'
+      xs={3}
+      sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
+          <Typography variant='h5' sx={{mb:1}}>Login</Typography>
+
+          <form>
+            <Grid container>
+              <Grid item xs={12} sx={{mt:2}}>
+                <TextField
+                label="Correo"
+                type="email"
+                placeholder='correo@google.com'
+                fullWidth
+                />
+              </Grid>
+              <Grid item sx={{mt:2}}>
+                <TextField
+                label="Contraseña"
+                type="password"
+                placeholder='contraseña'
+                fullWidth
+                />
+              </Grid>
+            </Grid>
+          </form>
+      </Grid>
+    </Grid>
   )
 }
 
