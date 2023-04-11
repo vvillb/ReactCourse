025070dd595d604a82a6fc0f673eaedaaa42e0ff
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-11 10:52:28 +0200

    SideBar

--- Journal/Journal-App/src/journal/layout/JournalLayout.jsx
+++ Journal/Journal-App/src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
 import {Box} from '@mui/system'
 import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
 
 
 const drawerWidth=240;
@@ -8,7 +9,7 @@ const JournalLayout = ({children}) => {
   return (
     <Box sx={{display:'flex'}}>
         <NavBar drawerWidth={drawerWidth}/>
-        {/**Sidebar */}
+        <SideBar drawerWidth={drawerWidth}/>
         <Box 
             component='main'
             sx={{flexGrow:1, p:3}}
